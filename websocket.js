187802b3064a48db19a1b7c5907224875bd885bd
arrasv2coder <pingpongttjournal@gmail.com>
2026-06-07 12:54:04 +0200

    Update websocket.js

--- websocket.js
+++ websocket.js
@@ -2,3 +2,5 @@ const crypto = require('crypto');
 const WebSocket = require('ws');
 const wss = new WebSocket.Server({ port: 8080 });
 console.log('WebSocket server is running on ws://localhost:8080');
+wss.on('connection', (ws) => {
+  console.log('New client connected');
