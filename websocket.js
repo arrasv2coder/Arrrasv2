454d9a7384832cca585bd3a1cbad216d7e38bc8e
arrasv2coder <pingpongttjournal@gmail.com>
2026-06-07 12:54:34 +0200

    Update websocket.js

--- websocket.js
+++ websocket.js
@@ -4,3 +4,4 @@ const wss = new WebSocket.Server({ port: 8080 });
 console.log('WebSocket server is running on ws://localhost:8080');
 wss.on('connection', (ws) => {
   console.log('New client connected');
+  ws.send("data");
