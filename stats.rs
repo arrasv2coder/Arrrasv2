75cb7fc3fee6fbf8ed6b14d549dc154865667efc
arrasv2coder <pingpongttjournal@gmail.com>
2026-06-08 16:40:02 +0200

    Update stats.rs
    Testing something

--- stats.rs
+++ stats.rs
@@ -6,7 +6,16 @@ struct Playerstats{
 }
 impl Playerstats{
   fn logmax(&self) {
-    let var = self.health;
+    let var = if self.health >= {1self.health} else {"1_f64"};
     var.log();
   }
+}
+//this part is just a test
+fn main() {
+  let mut stats=Playerstats {
+    health:"2_f64",
+    body_dmg:"1_f64",
+    bulletspeed:"3_f64",
+  };
+  stats.logmax();
 }
